4ef02b90f25349097b84ea6f8ab5cf102a577c9c
Erick Palma <gokuso03@gmail.com>
2019-08-19 23:49:02 -0600

    librerias css pasadas a local3

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/ASI-Conferencia/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = 'registro.php';
 
 /*
 |--------------------------------------------------------------------------
